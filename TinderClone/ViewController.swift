8f56a096a29cadadf7b83a7ad0d7e3715295e837
Petrus Carvalho <petrus.carvalho@conductor.com.br>
2020-03-14 20:00:54 -0300

    Initial Commit

---
+++ TinderClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderClone
+//
+//  Created by PETRUS CARVALHO on 14/03/20.
+//  Copyright © 2020 PETRUS CARVALHO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
